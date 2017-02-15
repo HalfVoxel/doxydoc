a440bac37959d958a0e64e7e75b4ad17ac5a5852
Aron Granberg <aron.granberg@gmail.com>
2017-02-15 13:20:32 +0100

    Code formatting.

--- plugins/searchengine/searchengine.py
+++ plugins/searchengine/searchengine.py
@@ -5,43 +5,44 @@ delimiter = "|$|"
 rowdelimiter = "\n"
 filename = "html/searchdata.csv"
 
+
 def generate_index():
+    f = file(filename, "w")
 
-	f = file(filename, "w")
+    for obj in Importer.iter_unique_docobjs():
+        try:
+            id = obj.full_url()
+            name = obj.name
+            desc = ""
+            compound = ""
+            kind = ""
+            if hasattr(obj, "briefdescription"):
+                Importer.pushwriterplain()
+                doxylayout.description(obj.briefdescription)
+                desc = Importer.popwriter()
 
-	for obj in Importer.iter_unique_docobjs():
-		try:
-			id = obj.full_url()
-			name = obj.name
-			desc = ""
-			compound = ""
-			kind = ""
-			if hasattr(obj, "briefdescription"):
-				Importer.pushwriterplain()
-				doxylayout.description(obj.briefdescription)
-				desc = Importer.popwriter()
+            if hasattr(obj, "compound") and obj.compound is not None:
+                compound = obj.compound.name
 
-			if hasattr(obj, "compound") and obj.compound is not None:
-				compound = obj.compound.name
+            if hasattr(obj, "kind") and obj.kind is not None:
+                kind = obj.kind
 
-			if hasattr(obj, "kind") and obj.kind is not None:
-				kind = obj.kind
+            f.write(id + delimiter + name + delimiter + desc + delimiter + compound + delimiter + kind + rowdelimiter)
+        except:
+            print(vars(obj))
+            raise
 
-			f.write(id + delimiter + name + delimiter + desc + delimiter + compound + delimiter + kind + rowdelimiter)
-		except:
-			print(vars(obj))
-			raise
+    f.close()
 
-	f.close()
 
 def searchbar():
-	Importer.writer.element("li")
-	Importer.writer.element("form", None, {"class": "navbar-search pull-right", "id": "searchform"})
-	Importer.writer.element("input", None, {"type": "text", "class": "search-query", "id": "searchfield", "placeholder": "Search", "data-toggle": "dropdown", "autocomplete": "off"})
-	Importer.writer.html("<ul class='dropdown-menu' role='menu' id='search-dropdown' aria-labelledby='dLabel'></ul>")
-	Importer.writer.element("/form")
-	Importer.writer.element("/li")
+    Importer.writer.element("li")
+    Importer.writer.element("form", None, {"class": "navbar-search pull-right", "id": "searchform"})
+    Importer.writer.element("input", None, {"type": "text", "class": "search-query", "id": "searchfield", "placeholder": "Search", "data-toggle": "dropdown", "autocomplete": "off"})
+    Importer.writer.html("<ul class='dropdown-menu' role='menu' id='search-dropdown' aria-labelledby='dLabel'></ul>")
+    Importer.writer.element("/form")
+    Importer.writer.element("/li")
+
 
 Importer.add_event(2600, generate_index)
-
 Importer.trigger_listener("navheader", 100, searchbar)
