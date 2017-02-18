83ced88efd9a3c58bd60ac04eee003c50255adc4
Aron Granberg <aron.granberg@gmail.com>
2017-02-18 19:48:52 +0100

    Updated external.csv.

--- unity-list.py
+++ unity-list.py
@@ -1,17 +1,19 @@
-import urllib2
+import urllib3
 import re
 
+
 def main():
-
-    urlbase = "http://docs.unity3d.com/Documentation/ScriptReference/"
-
-    f = urllib2.urlopen(urlbase + "20_class_hierarchy.html")
-    text = f.read()
+    urlbase = "https://docs.unity3d.com/ScriptReference/"
+    http = urllib3.PoolManager()
+    text = http.request('GET', urlbase + "docdata/toc.js").data.decode('utf-8')
 
     flist = open("external.csv", "w")
-    for match in re.finditer("<a\s+href=\"([^\"]*)\"\s+class=\"classlink\"\s*>(.*?)</a>", text):
-        flist.write(match.group(2) + ", " + urlbase + match.group(1) + "\n")
+    # for match in re.finditer("<a\s+href=\"([^\"]*)\"\s+class=\"classlink\"\s*>(.*?)</a>", text):
+    for match in re.finditer('"link":"(.+?)"', text):
+        if match.group(1) != "null":
+            flist.write(match.group(1) + ", " + urlbase + match.group(1) + ".html\n")
 
     flist.close()
+
 
 main()
