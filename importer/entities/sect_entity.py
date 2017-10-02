5fb262c8c4e517d2a656b574c269528fd2e60f76
Aron Granberg <aron.granberg@gmail.com>
2017-10-02 21:16:47 +0200

    Improve handling of section IDs

--- importer/entities/sect_entity.py
+++ importer/entities/sect_entity.py
@@ -1,10 +1,15 @@
 from .entity import Entity
-from typing import Dict
 import xml.etree.ElementTree as ET
 from importer.importer_context import ImporterContext
 
 
 class SectEntity(Entity):
+    def __init__(self) -> None:
+        super().__init__()
+        self.title = None  # type: ET.Element
+
     def read_from_xml(self, ctx: ImporterContext) -> None:
-        title = self.xml.find("title")
-        self.name = Entity.formatname(str(title.text))
+        self.title = self.xml.find("title")
+        # Doxygen generates the ID as "pagename_1realname"
+        # so we try to extract it here
+        self.name = self.id.split("_1")[-1]
