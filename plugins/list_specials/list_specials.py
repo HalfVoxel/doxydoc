a42884b78f1c5b601c950afb26f85ab682e0488a
Aron Granberg <aron.granberg@gmail.com>
2017-11-29 00:38:08 +0100

    Made more pages show up in the sidebar

--- plugins/list_specials/list_specials.py
+++ plugins/list_specials/list_specials.py
@@ -1,15 +1,16 @@
-from importer.entities import PageEntity, ClassEntity, GroupEntity, NamespaceEntity
+from importer.entities import PageEntity, ClassEntity, GroupEntity, NamespaceEntity, ExampleEntity
 from builder.entity_path import EntityPath
 from doxydoc_plugin import DoxydocPlugin
 
 
 class Plugin(DoxydocPlugin):
-    def pages_list(self, builder, importer):
+    def pages_list(self, builder, importer, entities, additional_pages):
         entity = PageEntity()
         entity.kind = "special"
         entity.id = "plugins/list_specials/pages"
         entity.short_name = entity.name = "Tutorials"
-        entity.innerpages = [e for e in importer.entities if e.kind == "page" and e.parent is None]
+        entity.innerpages = [e for e in entities if e.kind == "page" and e.parent is None]
+        entity.innerpages += additional_pages
         entity.path = EntityPath()
         importer._add_docobj(entity)
 
@@ -17,24 +18,24 @@ class Plugin(DoxydocPlugin):
             page.parent = entity
         return builder.page_generator._page_with_entity("special_pages_list", entity, [entity])
 
-    def classes_list(self, builder, importer):
+    def classes_list(self, builder, importer, entities):
         entity = GroupEntity()
         entity.kind = "special"
         entity.id = "plugins/list_specials/classes"
         entity.short_name = entity.name = "Classes"
-        entity.inner_classes = [e for e in importer.entities if isinstance(e, ClassEntity) and e.parent_in_canonical_path() is None]
-        entity.inner_namespaces = [e for e in importer.entities if isinstance(e, NamespaceEntity) and e.parent_in_canonical_path() is None]
+        entity.inner_classes = [e for e in entities if isinstance(e, ClassEntity) and e.parent_in_canonical_path() is None]
+        entity.inner_namespaces = [e for e in entities if isinstance(e, NamespaceEntity) and e.parent_in_canonical_path() is None]
         entity.path = EntityPath()
         importer._add_docobj(entity)
 
         return builder.page_generator._page_with_entity("special_class_list", entity, [entity])
 
-    def group_list(self, builder, importer):
+    def group_list(self, builder, importer, entities):
         entity = GroupEntity()
         entity.kind = "special"
         entity.id = "plugins/list_specials/groups"
         entity.short_name = entity.name = "Groups"
-        entity.inner_groups = [e for e in importer.entities if isinstance(e, GroupEntity)]
+        entity.inner_groups = [e for e in entities if isinstance(e, GroupEntity)]
         for e in entity.inner_groups:
             if e.parent is None:
                 e.parent = entity
@@ -44,5 +45,22 @@ class Plugin(DoxydocPlugin):
 
         return builder.page_generator.group_page(entity)
 
+    def examples_list(self, builder, importer, entities):
+        entity = GroupEntity()
+        entity.kind = "special"
+        entity.id = "plugins/list_specials/examples"
+        entity.short_name = entity.name = "Examples"
+        entity.inner_groups = [e for e in entities if isinstance(e, ExampleEntity)]
+
+        entity.path = EntityPath()
+        importer._add_docobj(entity)
+
+        return builder.page_generator.group_page(entity)
+
     def define_pages(self, importer, builder):
-        return [self.pages_list(builder, importer), self.classes_list(builder, importer), self.group_list(builder, importer)]
+        original_entities = importer.entities[:]
+        examples = self.examples_list(builder, importer, original_entities)
+        groups = self.group_list(builder, importer, original_entities)
+        classes = self.classes_list(builder, importer, original_entities)
+        pages = self.pages_list(builder, importer, original_entities, [p.primary_entity for p in [examples, groups, classes]])
+        return [pages, classes, groups, examples]
