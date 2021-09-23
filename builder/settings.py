a2ede28c2b8d7edecd7dae31d330fa4523df7b5a
Aron Granberg <aron.granberg@gmail.com>
2021-09-23 11:07:33 +0200

    Make Settings a dataclass

--- builder/settings.py
+++ builder/settings.py
@@ -1,33 +1,34 @@
 from typing import Any, List, Dict, Optional
 import os
+from dataclasses import dataclass
 
 
 class Settings:
+    hide_undocumented: bool = True
+    show_member_protection_in_list: bool = True
+    show_member_type_in_list: bool = True
+    show_source_files: bool = True
+    show_file_paths: bool = True
+    keep_empty_member_sections: bool = False
+    args: Any = None
+    in_dir: str = "input"
+    out_dir: str = "html"
+    template_dirs: List[str] = ["templates"]
+    title: str = "Project Title"
+    version: str = "0.1"
+    flat_file_hierarchy: bool = False
+    disabled_plugins: List[str] = []
+    page_whitelist: Optional[List[str]] = None
+    absolute_url_base: Optional[str] = None
+    banner: Optional[str] = None
+    breadcrumbs: bool = True
+    plugins: Optional[Dict[str,Any]] = None
+    doxygen_redirects: bool = False
+    doxygen_redirect_extension: str = ".html"
+    extra_css: Optional[str] = None
+    links_in_code_blocks = True
+    
     def __init__(self, settings_dict, paths_relative_to: str) -> None:
-        self.hide_undocumented = True
-        self.show_member_protection_in_list = True
-        self.show_member_type_in_list = True
-        self.show_source_files = True
-        self.show_file_paths = True
-        self.keep_empty_member_sections = False
-        self.args = None  # type: Any
-        self.in_dir = "input"
-        self.out_dir = "html"
-        self.template_dirs = ["templates"]
-        self.title = "Project Title"
-        self.version = "0.1"
-        self.flat_file_hierarchy = False
-        self.disabled_plugins = []  # type: List[str]
-        self.page_whitelist = None  # type: Optional[List[str]]
-        self.absolute_url_base = None  # type: Optional[str]
-        self.banner = None  # type: Optional[str]
-        self.breadcrumbs = True  # type: bool
-        self.plugins = None  # type: Optional[Dict[str,Any]]
-        self.doxygen_redirects = False
-        self.doxygen_redirect_extension = ".html"
-        self.extra_css = None  # type: Optional[str]
-        self.links_in_code_blocks = True
-
         if settings_dict is not None:
             for k, v in settings_dict.items():
                 if hasattr(self, k):
